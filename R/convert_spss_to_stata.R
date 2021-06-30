5a40fd56ba40ff09b2d15c63ee5219fe7cbb82e8
Daniel Morillo <danivmorillo@gmail.com>
2021-06-30 13:59:04 +0200

    There was a bug with the file name objects

--- R/convert_spss_to_stata.R
+++ R/convert_spss_to_stata.R
@@ -3,7 +3,7 @@ convert_spss_to_stata <- function(input_path, output_path) {
 
   message(TRANSFORMING_DATASET)
 
-  haven::write_dta(haven::read_sav(input_file), path = output_file)
+  haven::write_dta(haven::read_sav(input_path), path = output_path)
 
   message(DATASET_SAVED)
 
