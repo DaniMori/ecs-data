70f8fb4911f2da20316f733ab5bfa2d6b28c2bbd
Daniel Morillo <danivmorillo@gmail.com>
2021-12-13 15:53:33 +0100

    function `write_vars_table()` now returns tables in all the formats accepted by `pander.table()`

--- R/write_vars_table.R
+++ R/write_vars_table.R
@@ -3,29 +3,40 @@
 #' @importFrom assertive.types assert_is_data.frame
 #' @importFrom assertive.sets  assert_is_subset
 #' @importFrom pander          pandoc.table
-write_vars_table <- function(variables) {
+write_vars_table <- function(variables, style = "grid") {
 
   ## Constants: ----
-  VAR_PROPS <- c("var", "label", "type", "code")
-  HEADERS   <- c("Main variable name", "Label", "Format", "Code")
+  VAR_PROPS        <- c("var", "label", "type", "code")
+  HEADERS          <- c("Main variable name", "Label", "Format", "Code")
   names(VAR_PROPS) <- HEADERS
+
+  PANDOC_TABLE_STYLES <- c("multiline", "grid", "simple", "rmarkdown", "jira")
 
   ## Argument checking and formatting: ----
   assertive.types::assert_is_data.frame(variables)
   assertive.sets::assert_is_subset(VAR_PROPS, colnames(variables))
+  assertive.types::assert_is_a_string(style)
+
+  style <- match.arg(style, PANDOC_TABLE_STYLES)
 
   ## Main: ----
-  variables$code <- sapply(variables$code, format_var_code)
+  variables$code <- sapply(
+    variables$code,
+    format_var_code,
+    linefeeds = style == "grid"
+  )
 
   pander::pandoc.table(
     variables,
-    style   = "grid",
+    style   = style,
     justify = "llll",
     keep.line.breaks = TRUE
   )
 }
 
-format_var_code <- function(code) {
+format_var_code <- function(code, linefeeds = TRUE) {
 
-  paste('-', code, names(code), collapse = ";\n")
+  collapser <- if (linefeeds) ";\n" else "; "
+
+  paste('-', code, names(code), collapse = collapser)
 }
